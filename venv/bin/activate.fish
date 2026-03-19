a5ad5733d2efc997d5bc5e6b43335e87247f59fe
samugarciar <samugarciaro@outlook.com>
2026-03-19 14:11:30 -0500

    cambios

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/samug/Desktop/kommo_ia/venv"
+set -gx VIRTUAL_ENV "/Users/samug/GitHub/Da-os_cumbres_2026/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
